d59bf8c90c79cc23b0d760b53738a3fb3ef8a60e
ziddan <muhammadziddan580@gmail.com>
2023-06-15 22:45:12 +0700

    Initial commit

---
+++ game/script.js
@@ -0,0 +1,79 @@
+class Start {
+  constructor() {
+    this.playerName = "Player";
+    this.botName = "bot";
+    this.playerOption;
+    this.botOption;
+    this.winner = "";
+    this.score = 0;
+  }
+
+  get getBotOption() {
+    return this.botOption;
+  }
+
+  set setBotOption(option) {
+    this.botOption = option;
+  }
+
+  get getPlayerOption() {
+    return this.playerOption;
+  }
+
+  set setPlayerOption(option) {
+    this.playerOption = option;
+  }
+
+  botBrain() {
+    const option = ["🖐", "✌", "✊"];
+    const bot = option[Math.floor(Math.random() * option.length)];
+    return bot;
+  }
+
+  winCalculation() {
+    if (this.botOption == "🖐" && this.playerOption == "✌") {
+      return (this.winner = this.playerName);
+    } else if (this.botOption == "🖐" && this.playerOption == "✊") {
+      return (this.winner = this.botName);
+    } else if (this.botOption == "✌" && this.playerOption == "🖐") {
+      return (this.winner = this.botName);
+    } else if (this.botOption == "✌" && this.playerOption == "✊") {
+      return (this.winner = this.playerName);
+    } else if (this.botOption == "✊" && this.playerOption == "🖐") {
+      return (this.winner = this.playerName);
+    } else if (this.botOption == "✊" && this.playerOption == "✌") {
+      return (this.winner = this.botName);
+    } else {
+      return (this.winner = "SERI");
+    }
+  }
+
+  matchResult() {
+    if (this.winner != "SERI") {
+      return `${this.winner} MENANG!`;
+    } else {
+      return `WAAA ${this.winner}, GAK ADA YG MENANG 🤪`;
+    }
+  }
+  addScore() {
+    this.score++;
+  }
+}
+
+function pickOption(params) {
+  const start = new Start();
+  start.setPlayerOption = params;
+  start.setBotOption = start.botBrain();
+  start.winCalculation();
+
+  const inGame = document.getElementById("inGame");
+  const result = document.getElementById("result");
+
+  inGame.textContent = "...";
+  result.textContent = "...";
+
+  setTimeout(() => {
+    inGame.textContent = `${start.getPlayerOption} VS ${start.getBotOption}`;
+    result.textContent = start.matchResult();
+  }, 1500);
+}
